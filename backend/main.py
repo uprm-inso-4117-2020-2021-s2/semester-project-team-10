875c3ac4802f9e3833f7bf16ccb8255fd2c8560e
Samuel Diaz Bidot <samuel.diaz9@upr.edu>
2021-02-19 16:44:54 -0400

    Setup basic fastapi project

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.post("/login")
+def login():
+    #TODO
+    return "Login route"
+
+@app.post("/register")
+def register():
+    #TODO
+    return "Register route"
