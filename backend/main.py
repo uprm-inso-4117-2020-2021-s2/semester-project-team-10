dc2c27506cd8c6fda84b44ab022a4a3dc962c911
Chaliana Rolon <chaliana.rolon@upr.edu>
2021-03-02 19:28:09 -0400

    Added backend/main.py to tracked changes

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,27 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from models import user_model
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @app.get("/")
 def root():
@@ -9,7 +30,7 @@ def root():
 @app.post("/login")
 def login():
     #TODO
-    return "Login route"
+    return "Log in route"
 
 @app.post("/register")
 def register():
