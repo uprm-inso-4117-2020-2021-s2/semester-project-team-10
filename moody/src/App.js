e70d23bb8b95053bafee08cad84617f983bd7a34
Naomy Morales <naomy.morales@upr.edu>
2021-03-30 14:52:02 -0400

    Homepage functional

--- moody/src/App.js
+++ moody/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Icons from './components/Icons';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import Register from './pages/Register'
@@ -16,7 +17,9 @@ function App() {
           <Route path='/register' exact component={Register}/>
           <Route path='/signup' exact component={SignUp}/>
         </Switch>
-      </Router>  
+        <Icons /> 
+      </Router> 
+      
     </>
   );
 }
--- moody/src/pages/Home.js
+++ moody/src/pages/Home.js
@@ -2,50 +2,50 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+
 import '../App.css';
-
-
-// function Home() {
-//   return (
-//     <>
-//     </>
-//   );
-// }
 
 const useStyles=makeStyles((theme) => ({
   root: {
     height: '100vh',
+    display:'flex',
     justifyContent:"center",
     alignItems:'center'
   
   }, 
-  myimage: {
-       backgroundSize:'cover',
-       backgroundPosition:'center'
-  //   backgroundImage: `url(${process.env.PUBLIC_URL + "/assets/journal.png"})`,
-  //   backgroundRepeat: 'no-repeat',
-  //   backgroundSize: 'cover',
-  //   backgroundPosition: 'center',
-   },
+  // myimage: {
+  //      backgroundSize:'cover',
+  //      position:'center',
+  // //   backgroundImage: `url(${process.env.PUBLIC_URL + "/assets/journal.png"})`,
+  // //   backgroundRepeat: 'no-repeat',
+  // //   backgroundSize: 'cover',
+  // //   backgroundPosition: 'center',
+  //  },
 
   container:{
     textAlign:'center',
   },
-  title:{
-    color:'#030303',
-    fontSize:'3rem',
-    height:540
+  // title:{
+  //   color:'#030303',
+  //   fontSize:'3rem',
+  //   height:540
 
+  container2:{
+    position:'right',
+    left:360,
+    marginBottom:80
 
   },
+
+  // },
 
   sentence1:{
     background:'none',
     textAlign:'center',
     position:'absolute',
-    height: 238,
+    height:238,
     left:160,
-    top:610,
+    top:660,
     
 
   },
@@ -56,7 +56,7 @@ const useStyles=makeStyles((theme) => ({
     position:'absolute',
     height: 238,
     left:160,
-    top:660,
+    top:710,
 
   },
 
@@ -65,9 +65,31 @@ const useStyles=makeStyles((theme) => ({
     textAlign:'center',
     position:'absolute',
     height: 238,
-    left:160,
-    top:710,
+    left:160, 
+    top:760,
   },
+
+  container3:{
+    background:'none',
+    textAlign:'center',
+    position:'absolute',
+    height:238,
+    left:600,
+    top: 840,
+    fontSize:'3rem',
+    marginBottom:'50px',
+
+  },
+
+  step1:{
+    background:'none',
+    position:'absolute',
+    height:238,
+    left:160,
+    top: 960,
+
+  },
+
 
 }));
 
@@ -78,14 +100,14 @@ export default function Home(){
     <div className={classes.root}>
       <CssBaseline />
       
-      <img className="myimage"
+      {/* <img className="myimage"
         src={`${process.env.PUBLIC_URL}/assets/journal.png`}
         alt="logo"
         width="1520"
-        height="390"
-        />
+        height="360"
+        /> */}
     <div className={classes.container}>
-      <h1 className={classes.title}>About Moody</h1>
+      {/* <h1 className={classes.title}>About Moody</h1> */}
       <h1 className={classes.sentence1}>
         Moody is a personal journaling application that helps you express your inner feelings and thoughts.
       </h1>
@@ -94,10 +116,26 @@ export default function Home(){
     
       <h1 className={classes.sentence3}> secure and only for your eyes.
       </h1>
+      <h1 className={classes.container2}>
+      <img className="image2"
+        src={`${process.env.PUBLIC_URL}/assets/moodylogo4.png`}
+        alt="logo"
+        width="560"
+        height="560"
+        />
+      </h1>
+
+      <h1 className={classes.container3}> 
+       Getting started!
+      </h1>
+
+      
     </div>
     </div>
      
   );
+
+
 
 }
 
