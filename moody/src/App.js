bc0aa6f2bbdc6c70cec31c25a28673500c700ab2
dianelys.saldana <dianelys.saldana@upr.edu>
2021-03-31 13:32:26 -0400

    Added Icons import

--- moody/src/App.js
+++ moody/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Icons from './components/Icons'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import Register from './pages/Register'
