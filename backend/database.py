3de69ff4bba9bb02857821b497b3abe41f1aca97
Samuel Diaz Bidot <samuel.diaz9@upr.edu>
2021-07-04 13:10:50 -0400

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-db_url='database-1.cqooqygygpon.us-east-2.rds.amazonaws.com'
-db_name='postgres'
-db_username='moodydb'
-db_password='Group10SW'
+db_url='xxxxxxx'
+db_name='xxxxxx'
+db_username='xxxxxx'
+db_password='xxxxxx'
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{db_username}:{db_password}@{db_url}/{db_name}'
 
